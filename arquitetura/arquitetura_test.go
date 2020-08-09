1543b58b2aca5213afaba4174807e10ede25703d
samuelmachado <samuel.silva39@fatec.sp.gov.br>
2020-08-09 14:05:50 -0300

    transaction + database

---
+++ arquitetura/arquitetura_test.go
@@ -0,0 +1,15 @@
+package arquitetura
+
+import (
+	"runtime"
+	"testing"
+)
+
+func TestDependente(t *testing.T) {
+	t.Parallel()
+	if runtime.GOARCH == "amd64" {
+		t.Skip("Não funciona em arq. amd64")
+	}
+
+	t.Fail()
+}
